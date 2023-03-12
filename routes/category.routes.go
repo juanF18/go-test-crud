fba1c777d6cc10a1a131d416e0c3d8ab42e335fc
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2023-03-11 21:30:33 -0500

    Se agrega las rutas principales y el README.md

--- routes/category.routes.go
+++ routes/category.routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import "net/http"
+
+func GetCategorysController(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetCategoryController(w http.ResponseWriter, r *http.Request) {
+
+}
+func CreateCategorysController(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeleteCategorysController(w http.ResponseWriter, r *http.Request) {
+
+}
--- routes/product.routes.go
+++ routes/product.routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import "net/http"
+
+func GetUsersController(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetUserController(w http.ResponseWriter, r *http.Request) {
+
+}
+func CreateUserController(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeleteUserController(w http.ResponseWriter, r *http.Request) {
+
+}
