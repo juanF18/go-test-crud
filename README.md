ca5ddff052f90518100c257185c165b26c87f12e
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-03-19 16:40:18 -0500

    Se agregan las rutas de productos y categoria

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-## Hola mundo
+## Go test crud
+
+### Herramientas que se utilizaron
+
+Estas fueron las herramientas que se utilizar para implementar un crud basico en go
+
+- go
+- gorm
+- mux
+
+## Para inicial el proyecto
+
+Primero se tiene que tener instalado go en la version 1.20.2, luego hacer uso de `go get .` para instalar los paquetes necesario (gorm, mux).
+
+- `go get .`
+- `go run main.go` o si tiene instalado el paquete para hot reload que es air usar el comando `air .`
--- routes/index.routes.go
+++ routes/index.routes.go
@@ -9,6 +9,8 @@ func NewRoutes() *mux.Router {
 	r := mux.NewRouter()
 	handleRouteHome(r)
 	handleRoutesUser(r)
+	handleRouteProducts(r)
+	handleRouteCategory(r)
 	return r
 }
 
@@ -21,5 +23,18 @@ func handleRoutesUser(r *mux.Router) {
 	r.HandleFunc("/users/{id}", controller.GetUserController).Methods("GET")
 	r.HandleFunc("/users", controller.CreateUsersController).Methods("POST")
 	r.HandleFunc("/user/{id}", controller.DeleteUsersController).Methods("DELETE")
+}
 
+func handleRouteProducts(r *mux.Router) {
+	r.HandleFunc("/producs", controller.GetProductsController).Methods("GET")
+	r.HandleFunc("/producs/{id}", controller.GetProductController).Methods("GET")
+	r.HandleFunc("/producs", controller.CreateProductController).Methods("POST")
+	r.HandleFunc("/producs/{id}", controller.DeleteProductController).Methods("DELETE")
+}
+
+func handleRouteCategory(r *mux.Router) {
+	r.HandleFunc("/category", controller.GetCategorysController).Methods("GET")
+	r.HandleFunc("/category/{id}", controller.GetCategoryController).Methods("GET")
+	r.HandleFunc("/category", controller.CreateCategorysController).Methods("POST")
+	r.HandleFunc("/category/{id}", controller.DeleteCategorysController).Methods("DELETE")
 }
